e022af4ab754a853330c85bc4c3fa4e942735305
winner <1750110604@qq.com>
2018-05-03 14:59:54 +0800

    质量群，普通群

---
+++ js/commonGroup.js
@@ -0,0 +1,72 @@
+$(function(){
+	getTestTk();
+
+	//行业选择
+	$('button#indu').on('click', function(event) {
+
+	});
+	//地区选择
+	$('button#addr').on('click', function(event) {
+		//跳查找群
+		window.location.href='searchQun.html';
+	});
+	//质量群
+	$('button#massGroup').on('click', function(event) {
+		window.location.href='massGroup.html';
+	});
+	//查看下一个
+	$('button#lookNext').on('click', function(event) {
+		getQRCode(tk,1);
+	});
+})
+var tk = '';
+
+/**
+ * 获取测试tk
+ */
+function getTestTk(){
+	$.ajax({
+	  url: 'http://47.104.218.168:8117/2?dv=1',
+	  type: 'get',
+	  dataType: 'json',
+	  success: function (data) {
+	    console.log(data);
+	    tk = data.ret[0].d.tk;
+	    getQRCode(tk,0);
+	  },
+	  fail: function (err) {
+	    console.log(err)
+	  }
+	})	
+}
+/**
+ * 获取数据
+ */
+function getQRCode(tk,type){
+	$.ajax({
+	  url: 'http://47.104.218.168:8117/4?tk='+tk,
+	  type: 'get',
+	  dataType: 'json',
+	  success: function (data) {
+	    console.log(data);
+	    if (data.ret[0].d.length==1) {
+			$('#QRCode').html('<img src="'+data.ret[0].d[0].qr_url+'" alt="二维码">');
+	    } else {
+	    	$('#QRCode').html('<img src="'+data.ret[0].d[type].qr_url+'" alt="二维码">');
+	    }
+
+	  },
+	  fail: function (err) {
+	    console.log(err)
+	  }
+	})		
+}
+
+/**
+ * 获取url中的参数
+ */
+function getQueryString(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
+	var r = window.location.search.substr(1).match(reg); 
+	if (r != null) return unescape(r[2]); return null; 
+}
---
+++ js/massGroup.js
@@ -0,0 +1,78 @@
+$(function(){
+	getTestTk();
+
+	//行业选择
+	$('button#indu').on('click', function(event) {
+
+	});
+	//地区选择
+	$('button#addr').on('click', function(event) {
+		//跳查找群
+		window.location.href='searchQun.html';
+	});
+	//质量群
+	$('button#commonGroup').on('click', function(event) {
+		window.location.href='commonGroup.html';
+	});
+	//查看下一个
+	$('button#lookNext').on('click', function(event) {
+		getQRCode(tk,industry,area);
+	});
+
+
+
+
+})
+var tk = '', industry= '', area = '';
+/**
+ * 获取测试tk
+ */
+function getTestTk(){
+	$.ajax({
+	  url: 'http://47.104.218.168:8117/2?dv=1',
+	  type: 'get',
+	  dataType: 'json',
+	  success: function (data) {
+	    console.log(data);
+	    tk = data.ret[0].d.tk;
+	    industry = getQueryString('industry');
+	    if (industry==null) {
+	    	industry = '';
+	    }
+	    area = getQueryString('area');
+	    if (area == null) {
+	    	area = '';
+	    }
+	    getQRCode(tk,industry,area);
+	  },
+	  fail: function (err) {
+	    console.log(err)
+	  }
+	})	
+}
+
+/**
+ * 获取数据
+ */
+function getQRCode(tk,industry,area){
+	$.ajax({
+	  url: 'http://47.104.218.168:8117/5?tk='+tk+'&industry='+industry+'&area='+area,
+	  type: 'get',
+	  dataType: 'json',
+	  success: function (data) {
+	    console.log(data);
+	    $('#QRCode').html('<img src="'+data.ret[0].d.qr_url+'" alt="二维码">');
+	  },
+	  fail: function (err) {
+	    console.log(err)
+	  }
+	})		
+}
+/**
+ * 获取url中的参数
+ */
+function getQueryString(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
+	var r = window.location.search.substr(1).match(reg); 
+	if (r != null) return unescape(r[2]); return null; 
+}
